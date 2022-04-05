3e66d485b8063af84a1fbac4524ff737c2c04533
ehanschen <ehanschen@3pc.de>
2022-04-05 13:43:12 +0200

    add not found component and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Home from '../views/Home.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -25,6 +26,21 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true,
+  },
+  // catch-all 404 example
+  // will catch any routes that are not covered by routes defined in this file
+  // it's a REGEX, not a function
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
+  // redirect example
+  {
+    // that's what the route used to be:
+    path: '/all-jobs',
+    // that's where we want it to go:
+    redirect: '/jobs',
   },
 ]
 
